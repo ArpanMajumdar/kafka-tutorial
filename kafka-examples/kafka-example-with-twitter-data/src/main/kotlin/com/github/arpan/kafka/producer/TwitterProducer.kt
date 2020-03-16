1950feb279a51fc9904d0553f8bcb647e32e130f
Arpan Majumdar <arpan.majumdar.dev@gmail.com>
2020-03-16 21:03:12 +0530

    Reading tweets from twitter done

--- kafka-examples/kafka-example-with-twitter-data/src/main/kotlin/com/github/arpan/kafka/producer/TwitterProducer.kt
+++ kafka-examples/kafka-example-with-twitter-data/src/main/kotlin/com/github/arpan/kafka/producer/TwitterProducer.kt
@@ -1,5 +1,24 @@
 package com.github.arpan.kafka.producer
 
+import java.util.concurrent.LinkedBlockingQueue
+import java.util.concurrent.TimeUnit
+
 fun main() {
-    println("Hello world")
+    val consumerKey: String = System.getenv("CONSUMER_KEY")
+            ?: throw IllegalArgumentException("CONSUMER_KEY is required.")
+    val consumerSecret: String = System.getenv("CONSUMER_SECRET")
+            ?: throw IllegalArgumentException("CONSUMER_SECRET is required.")
+    val accessToken: String = System.getenv("ACCESS_TOKEN")
+            ?: throw IllegalArgumentException("ACCESS_TOKEN is required.")
+    val accessTokenSecret: String = System.getenv("ACCESS_TOKEN_SECRET")
+            ?: throw IllegalArgumentException("ACCESS_TOKEN_SECRET is required.")
+
+    val msgQueue = LinkedBlockingQueue<String>(1000)
+    val twitterAuth = TwitterAuth(consumerKey, consumerSecret, accessToken, accessTokenSecret)
+    val terms = listOf("corona")
+
+    TwitterClient(msgQueue, twitterAuth, terms).apply {
+        connect()
+        pollForDuration(5, TimeUnit.SECONDS)
+    }
 }
